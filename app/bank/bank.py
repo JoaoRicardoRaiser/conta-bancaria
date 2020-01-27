43dd9b6f82918ec21c7fac9ae1cb6ac8371b831d
guilherme.kramer <Kat272666@>
2020-01-27 12:30:57 -0300

    a

--- app/bank/bank.py
+++ app/bank/bank.py
@@ -2,9 +2,13 @@ from app.user.User import User
 
 
 class Bank:
-    def __init__(self):
+    def __init__(self, name):
+        self._name = name
         self._accounts = []
         self._users_accounts_registration = []
+
+    def get_bank_name(self):
+        return self._name
 
     def create_login(self) -> str:
         pass
